156e87188228f3a8246e182ef0ec0dd9b93b88e0
Cluttered04 <sbpwem@gmail.com>
2019-05-16 13:36:17 -0400

    Added first set of models

--- TestBangazonAPI/UnitTest1.cs
+++ TestBangazonAPI/UnitTest1.cs
@@ -1,5 +1,12 @@
-using System;
+using Newtonsoft.Json;
+using BangazonAPI.Models;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
+using System.Linq;
 
 namespace TestBangazonAPI
 {
