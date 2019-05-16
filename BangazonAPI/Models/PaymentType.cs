15b622e8fdc14378c6dfe24608377553d42c3a54
sydneywait <sydneywait@gmail.com>
2019-05-16 14:00:37 -0400

    added Connot's models

--- BangazonAPI/Models/PaymentType.cs
+++ BangazonAPI/Models/PaymentType.cs
@@ -5,10 +5,15 @@ using System.Threading.Tasks;
 
 namespace BangazonAPI.Models
 {
-    public class ProductType
+    public class PaymentType
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
+        public int AcctNumber { get; set; }
+
         public string Name { get; set; }
+
+        public int CustomerId { get; set; }
+
         public bool IsActive { get; set; }
     }
 }
--- BangazonAPI/Models/Product.cs
+++ BangazonAPI/Models/Product.cs
@@ -14,7 +14,7 @@ namespace BangazonAPI.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public int Quantity { get; set; }
-        public bool isActive { get; set; }
+        public bool IsActive { get; set; }
 
     }
 }
