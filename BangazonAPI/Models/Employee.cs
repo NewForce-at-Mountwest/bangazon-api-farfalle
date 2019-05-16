2602af5eec20d92dac72f90e613e80f7478a565e
Cluttered04 <sbpwem@gmail.com>
2019-05-16 13:54:53 -0400

    Fixed missing primary key on employee model

--- BangazonAPI/Models/Employee.cs
+++ BangazonAPI/Models/Employee.cs
@@ -7,6 +7,7 @@ namespace BangazonAPI.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int DepartmentId { get; set; }
