96cff90bff992ddba8cb7351c01c9094e74c5289
Cluttered04 <sbpwem@gmail.com>
2019-05-16 15:10:27 -0400

    changed the employee list's variable name to better follow naming scheme in other models

--- BangazonAPI/Models/Department.cs
+++ BangazonAPI/Models/Department.cs
@@ -11,6 +11,6 @@ namespace BangazonAPI.Models
         public string Name { get; set; }
         public int Budget { get; set; }
 
-        public List<Employee> EmployeeList { get; set; } = new List<Employee>();
+        public List<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
