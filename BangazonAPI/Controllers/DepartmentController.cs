f6e63793e6cc5d4fad72000af44ffb6c90f4b983
sydneywait <sydneywait@gmail.com>
2019-05-17 15:40:42 -0400

    added post and put to the departments controller

--- BangazonAPI/Controllers/DepartmentController.cs
+++ BangazonAPI/Controllers/DepartmentController.cs
@@ -56,8 +56,7 @@ namespace BangazonAPI.Controllers
                     {
                         commandText+= $" WHERE d.budget > '{_gt}'"; 
 
-                    }
-                    
+                    }                   
 
                     cmd.CommandText = commandText;
 
@@ -79,14 +78,21 @@ namespace BangazonAPI.Controllers
                             Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
 
                         };
-                        employee = new Employee
-                        {
-                            Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
-                            FirstName = reader.GetString(reader.GetOrdinal("Employee FirstName")),
-                            LastName = reader.GetString(reader.GetOrdinal("Employee LastName")),
-                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
-                            IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("isSuperVisor"))
-                        };
+
+                        //Check first to see if this line on the table has an employee in it so the code doesn't throw an error
+                        if (!reader.IsDBNull(reader.GetOrdinal("EmployeeId"))) {
+                            employee = new Employee
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("Employee FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("Employee LastName")),
+                                DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("isSuperVisor"))
+                            };
+                         
+                            
+                        }
+                        else { employee = null; }
 
                         if (departments.Any(d => d.Id == department.Id))
                         {
@@ -96,21 +102,19 @@ namespace BangazonAPI.Controllers
 
                                 if (!departmentOnList.Employees.Any(e => e.Id == employee.Id))
                                 {
-                                    departmentOnList.Employees.Add(employee);
-
+                                        departmentOnList.Employees.Add(employee);
                                 }
                             }                           
 
                         }
                         else
                         {
-
+                            
                             if (_include == "employees") {
                                 department.Employees.Add(employee);
                                 }
 
                                 departments.Add(department);
-
                         }
 
                     }
@@ -125,133 +129,151 @@ namespace BangazonAPI.Controllers
 
 
 
-        //        //GET: Code for getting a single department (active or not)
-        //        [HttpGet("{id}", Name = "Department")]
-        //        public async Task<IActionResult> GetSingleDepartment([FromRoute] int id)
-        //        {
-        //            using (SqlConnection conn = Connection)
-        //            {
-        //                conn.Open();
-        //                using (SqlCommand cmd = conn.CreateCommand())
-        //                {
-        //                    cmd.CommandText = $"SELECT Id, AcctNumber, [Name], CustomerId, IsActive from Department WHERE Id=@id";
-        //                    cmd.Parameters.Add(new SqlParameter("@id", id));
-        //                    SqlDataReader reader = cmd.ExecuteReader();
+        //GET: Code for getting a single department (active or not)
+        [HttpGet("{id}", Name = "Department")]
+        public async Task<IActionResult> GetSingleDepartment([FromRoute] int id, string _include)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = $"SELECT d.Id as 'DepartmentId', d.[Name] AS 'Department Name', d.Budget, e.id as 'EmployeeId', e.FirstName as 'Employee FirstName', e.LastName as 'Employee lastName', e.IsSuperVisor FROM Department d Full JOIN Employee e on d.id = e.departmentId WHERE d.Id=@id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Employee employee = null;
+                    Department departmentToDisplay = null;
 
-        //                    Department departmentToDisplay = null;
+                    int counter = 0;
 
-        //                    while (reader.Read())
-        //                    {
+                    while (reader.Read())
+                    {
 
-        //                        {
-        //                            departmentToDisplay = new Department
-        //                            {
-        //                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
-        //                                AcctNumber = reader.GetInt32(reader.GetOrdinal("AcctNumber")),
-        //                                Name = reader.GetString(reader.GetOrdinal("Name")),
-        //                                CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
-        //                                IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive"))
-
-        //                            };
-        //                        };
-        //                    };
-
-
-        //                    reader.Close();
-
-        //                    return Ok(departmentToDisplay);
-        //                }
-        //            }
-        //        }
-
-        //        //  POST: Code for creating a department
-        //        [HttpPost]
-        //        public async Task<IActionResult> PostDepartment([FromBody] Department department)
-        //        {
-        //            using (SqlConnection conn = Connection)
-        //            {
-        //                conn.Open();
-        //                using (SqlCommand cmd = conn.CreateCommand())
-        //                {
-        //                    cmd.CommandText = @"INSERT INTO Department (AcctNumber, [Name], CustomerId, IsActive)
-        //                                                    OUTPUT INSERTED.Id
-        //                                                    VALUES (@AcctNumber, @Name, @CustomerId, @IsActive)";
-        //                    cmd.Parameters.Add(new SqlParameter("@AcctNumber", department.AcctNumber));
-        //                    cmd.Parameters.Add(new SqlParameter("@Name", department.Name));
-        //                    cmd.Parameters.Add(new SqlParameter("@CustomerId", department.CustomerId));
-        //                    cmd.Parameters.Add(new SqlParameter("@IsActive", department.IsActive));
+                        if (counter < 1)
+                        {
+                            departmentToDisplay = new Department
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                Name = reader.GetString(reader.GetOrdinal("Department Name")),
+                                Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                            };
+                            counter++;
+                        }
 
 
+                        if (_include == "employees")
+                        {
+                            //Check to see that the current employee is not null
+                            if (!reader.IsDBNull(reader.GetOrdinal("EmployeeId")))
+                            {
+                                employee = new Employee
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("Employee FirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("Employee LastName")),
+                                    DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                    IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("isSuperVisor"))
+                                };
+                                departmentToDisplay.Employees.Add(employee);
 
-        //                    int newId = (int)cmd.ExecuteScalar();
-        //                    department.Id = newId;
-        //                    return CreatedAtRoute("Department", new { id = newId }, department);
-        //                }
-        //            }
-        //        }
-
-        //        // PUT: Code for editing a department
-        //        [HttpPut("{id}")]
-        //        public async Task<IActionResult> PutDepartment([FromRoute] int id, [FromBody] Department department)
-        //        {
-        //            try
-        //            {
-        //                using (SqlConnection conn = Connection)
-        //                {
-        //                    conn.Open();
-        //                    using (SqlCommand cmd = conn.CreateCommand())
-        //                    {
-        //                        cmd.CommandText = @"UPDATE Department
-        //                                                        SET AcctNumber = @AcctNumber,
-        //                                                        Name = @Name,
-        //                                                        Budget=@Budget"
+                            }
+                        }
+                            
+                        
+                    };
 
 
-        //                        cmd.Parameters.Add(new SqlParameter("@Name", department.Name));
-        //                        cmd.Parameters.Add(new SqlParameter("@Budget", department.Budget));
-        //                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                    reader.Close();
 
-        //                        int rowsAffected = cmd.ExecuteNonQuery();
-        //                        if (rowsAffected > 0)
-        //                        {
-        //                            return new StatusCodeResult(StatusCodes.Status204NoContent);
-        //                        }
-        //                        throw new Exception("No rows affected");
-        //                    }
-        //                }
-        //            }
-        //            catch (Exception)
-        //            {
-        //                if (!DepartmentExists(id))
-        //                {
-        //                    return NotFound();
-        //                }
-        //                else
-        //                {
-        //                    throw;
-        //                }
-        //            }
-        //        }
+                    return Ok(departmentToDisplay);
+                }
+            }
+        }
+
+        //  POST: Code for creating a department
+        [HttpPost]
+        public async Task<IActionResult> PostDepartment([FromBody] Department department)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Department (Name, Budget)
+                                                            OUTPUT INSERTED.Id
+                                                            VALUES (@Name, @Budget)";
+                    cmd.Parameters.Add(new SqlParameter("@Name", department.Name));
+                    cmd.Parameters.Add(new SqlParameter("@Budget", department.Budget));
+                   
+                    int newId = (int)cmd.ExecuteScalar();
+                    department.Id = newId;
+                    return CreatedAtRoute("Department", new { id = newId }, department);
+                }
+            }
+        }
+
+        // PUT: Code for editing a department
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDepartment([FromRoute] int id, [FromBody] Department department)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Department
+                                           SET AcctNumber = @AcctNumber,
+                                           Name = @Name,
+                                           Budget = @Budget";
+
+
+                        cmd.Parameters.Add(new SqlParameter("@Name", department.Name));
+                        cmd.Parameters.Add(new SqlParameter("@Budget", department.Budget));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
 
 
 
-        //        private bool DepartmentExists(int id)
-        //        {
-        //            using (SqlConnection conn = Connection)
-        //            {
-        //                conn.Open();
-        //                using (SqlCommand cmd = conn.CreateCommand())
-        //                {
-        //                    cmd.CommandText = @"SELECT Id, name
-        //                                    FROM Department
-        //                                    WHERE Id = @id";
-        //                    cmd.Parameters.Add(new SqlParameter("@id", id));
+        private bool DepartmentExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name
+                                            FROM Department
+                                            WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
 
-        //                    SqlDataReader reader = cmd.ExecuteReader();
-        //                    return reader.Read();
-        //                }
-        //            }
-        //        }
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
 
 
     }
